b5bdb59b3d36be8563c29adb12e0850eb3425f1d
Reeti <reetm@me.com>
2022-03-14 12:39:11 -0400

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product_1 = Product.new({
+  name: "apple",
+  price: 2,
+  image_url: "https://media.istockphoto.com/photos/red-apple-with-leaf-isolated-on-white-background-picture-id185262648?b=1&k=20&m=185262648&s=170667a&w=0&h=2ouM2rkF5oBplBmZdqs3hSOdBzA4mcGNCoF2P0KUMTM=",
+  description: "red fruit",
+})
+
+product_2 = Product.new({
+  name: "banana",
+  price: 4,
+  image_url: "https://cdn1.sph.harvard.edu/wp-content/uploads/sites/30/2018/08/bananas-1354785_1920-1200x800.jpg",
+  description: "yellow fruit",
+})
